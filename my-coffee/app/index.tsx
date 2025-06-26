cd2ad96c238a5ec869ad15b20a80ede73f258d95
akiran1sei <nakamori.1224@gmail.com>
2025-06-26 11:25:36 +0900

    設定メニュー等作成

--- my-coffee/app/index.tsx
+++ my-coffee/app/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { Link } from "expo-router";
 import { IconButton } from "react-native-paper";
 import * as Font from "expo-font"; // expo-fontをインポート
@@ -7,6 +7,11 @@ import "react-native-get-random-values";
 import { LoadingComponent } from "@/components/MessageComponent";
 export default function Index() {
   const [loading, setLoading] = useState(false);
+  const [switchState, setSwitchState] = useState(false); // switchの状態を管理するためのuseStateフックを追加
+  const handleSwitch = () => {
+    setSwitchState(!switchState); // switchの状態を反転させる
+  };
+
   useEffect(() => {
     // 副作用の処理
     async function loadFonts() {
@@ -40,13 +45,45 @@ export default function Index() {
                 <IconButton icon="view-list" size={50} iconColor="#D2B48C" />
               </Link>
             </View>
-            <View style={styles.settingButtonContainer}>
-              <Link
-                href="/settings/PrivacyPolicyJP"
-                style={styles.settingButton}
-              >
-                <IconButton icon="cog" size={30} iconColor="#D2B48C" />
-              </Link>
+            <View style={styles.settingContainer}>
+              {switchState ? (
+                <View style={styles.settingContainerWrap}>
+                  <TouchableOpacity>
+                    <Link
+                      href="/settings/PrivacyPolicyJP"
+                      style={styles.settingButton}
+                    >
+                      <Text style={styles.settingButtonText}>
+                        プライバシーポリシー
+                      </Text>
+                    </Link>
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Link
+                      href="/settings/TermsAndConditionsJP"
+                      style={styles.settingButton}
+                    >
+                      <Text style={styles.settingButtonText}>利用規約</Text>
+                    </Link>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.settingButtonClose}
+                    onPress={() => {
+                      handleSwitch();
+                    }}
+                  >
+                    <IconButton icon="close" size={30} iconColor="#f5f5f5" />
+                  </TouchableOpacity>
+                </View>
+              ) : (
+                <TouchableOpacity
+                  onPress={() => {
+                    handleSwitch();
+                  }}
+                >
+                  <IconButton icon="cog" size={30} iconColor="#D2B48C" />
+                </TouchableOpacity>
+              )}
             </View>
           </View>
         </View>
@@ -60,6 +97,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F5F5F5",
   },
+
   contents: {
     flex: 1,
   },
@@ -87,19 +125,41 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginVertical: 10,
   },
-  settingButtonContainer: {
+  settingContainer: {
     position: "absolute",
     alignItems: "center",
-    bottom: 10,
-    right: 10,
+    bottom: 0,
+    right: 0,
     justifyContent: "center",
+  },
+
+  settingContainerWrap: {
+    backgroundColor: "#D2B48C",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    flexDirection: "column",
+    alignItems: "center",
+    position: "relative",
+    width: "auto",
+    height: "auto",
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    paddingBottom: 60,
   },
   settingButton: {
     textDecorationLine: "underline",
-
     borderRadius: 10,
     marginVertical: 5,
     marginTop: 20,
+  },
+  settingButtonClose: {
+    position: "absolute",
+    bottom: 0,
+    right: 10,
+  },
+  settingButtonText: {
+    color: "#f5f5f5",
+    fontSize: 16,
   },
 
   pageTitle: { width: "100%", height: 80 },
